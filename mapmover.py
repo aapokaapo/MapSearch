aea0fff79f6fbc0d920ba55ebf238bad6d1f68fc
aapokaapo <aapok98@gmail.com>
2020-03-22 14:03:49 +0200

    MapSearch v0.51

--- mapmover.py
+++ mapmover.py
@@ -4,27 +4,31 @@ import shutil
 
 async def move_to(map_memory, keyword, type):
     """
+    map_memory: type: list
     keyword: type: str
+    type: type: str
     """
     found = False
     message = "Couldn't find the map you're trying to move!"
+    # check if the map user tries to move is in memory
     for map in map_memory:
         if map.name.split('/')[-1] == keyword:
             found = True
             mapname = map.name
+            # rename the map in memory with new prefix
             map.name = type + "/" + keyword.split('/')[-1]
-        else:
-            pass
 		
-    if found:
-        message = "{} already in {}™".format(keyword, type)  
-        if not os.path.exists("{}{}/{}.bsp".format(path, type, keyword)):
-            message = "{} moved to {}™!".format(keyword, type)        
-            shutil.move("{}{}.bsp".format(path, mapname), "{}{}/{}.bsp".format(path, type, keyword))
-            try:
-                shutil.move("{}{}.jpg".format(mapshot_path, mapname), "{}{}/{}.jpg".format(mapshot_path, type, keyword))
-            except FileNotFoundError:
-                pass
+        if found:
+            message = "{} already in {}™".format(keyword, type)
+            # check if the file is already in the dir user tries to move in
+            if not os.path.exists("{}{}/{}.bsp".format(path, type, keyword)):
+                message = "{} moved to {}™!".format(keyword, type)        
+                shutil.move("{}{}.bsp".format(path, mapname), "{}{}/{}.bsp".format(path, type, keyword))
+                # if there is a mapshot for the map, move it too
+                try:
+                    shutil.move("{}{}.jpg".format(mapshot_path, mapname), "{}{}/{}.jpg".format(mapshot_path, type, keyword))
+                except FileNotFoundError:
+                    pass
         
     return message
     
